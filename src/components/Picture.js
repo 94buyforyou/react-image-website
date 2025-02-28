dd03ecedd69cc88e79b30e3cf15901138d05a325
Heartscars <onlybuyforyou@gmail.com>
2025-02-28 17:05:43 +0800

    搜尋圖片功能

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -5,7 +5,7 @@ const Picture = ({ data }) => {
         <div className="picture">
             <p>{data.photographer}</p>
             <div className="imageContainer">
-                <img src={data.src.large} alt="" />
+                <img src={data.src.large} alt={data.alt} />
             </div>
             <p>
                 在此下載圖片:
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,18 +4,31 @@ import Picture from "../components/Picture";
 import axios from "axios";
 
 export const Homepage = () => {
+    let [input, setInput] = useState("");
     let [data, setData] = useState(null);
     const auth = process.env.REACT_APP_API_KEY;
     const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+    let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
 
-    const search = async () => {
-        let result = await axios.get(initialURL, { headers: { Authorization: auth } });
+    const search = async (url) => {
+        let result = await axios.get(url, { headers: { Authorization: auth } });
         console.log(result);
         setData(result.data.photos);
     };
+
+    // 當進入這個頁面時,自動search圖片
+    useEffect(() => {
+        search(initialURL);
+    }, []);
+
     return (
         <div style={{ minHeight: "100vh" }}>
-            <Search search={search} />
+            <Search
+                search={() => {
+                    search(searchURL);
+                }}
+                setInput={setInput}
+            />
             <div className="pictures">
                 {data &&
                     data.map((d) => {
