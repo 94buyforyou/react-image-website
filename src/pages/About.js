5f7b6879a80a379efbf71c2adb40050e0c8c679c
Heartscars <onlybuyforyou@gmail.com>
2025-02-28 22:40:14 +0800

    react route測試

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 export const About = () => {
-    return <div>About</div>;
+    return (
+        <div>
+            <h1>這裡是About</h1>
+        </div>
+    );
 };
 
 export default About;
--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 export const Page404 = () => {
-    return <div>Page404</div>;
+    return (
+        <div>
+            <h1>這裡是404</h1>
+        </div>
+    );
 };
 
 export default Page404;
