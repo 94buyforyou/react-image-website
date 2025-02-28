cbc506f02038f59000f205094366f3fedfab2b5a
Heartscars <onlybuyforyou@gmail.com>
2025-02-28 16:43:39 +0800

    展示圖片設定

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,6 @@
-import React from "react";
-import axios from "axios";
-require("dotenv").config();
+import React, { useState } from "react";
 
-const Search = () => {
-    const auth = process.env.REACT_APP_API_KEY;
-    const initalURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-
-    const search = async () => {
-        let result = await axios.get(initalURL, { headers: { Authorization: auth } });
-        console.log(result);
-    };
-
+const Search = ({ search }) => {
     return (
         <div className="search">
             <input className="input" type="text" />
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Search from "../components/Search";
+import Picture from "../components/Picture";
 import axios from "axios";
 
 export const Homepage = () => {
+    let [data, setData] = useState(null);
+    const auth = process.env.REACT_APP_API_KEY;
+    const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+
+    const search = async () => {
+        let result = await axios.get(initialURL, { headers: { Authorization: auth } });
+        console.log(result);
+        setData(result.data.photos);
+    };
     return (
         <div style={{ minHeight: "100vh" }}>
-            <Search />
+            <Search search={search} />
+            <div className="pictures">
+                {data &&
+                    data.map((d) => {
+                        return <Picture data={d} />;
+                    })}
+            </div>
         </div>
     );
 };
